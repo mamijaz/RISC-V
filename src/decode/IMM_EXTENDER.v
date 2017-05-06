2b7127b1b0f9bdab099669c240e331b00e308d93
mamijaz <ihjaaz@gmail.com>
2017-05-06 08:57:41 +0530

    Created Mux_2_to_1

--- src/decode/IMM_EXTENDER.v
+++ src/decode/IMM_EXTENDER.v
@@ -5,7 +5,7 @@
 // 
 // Create Date:     05/05/2017 10:52:10 AM
 // Design Name: 
-// Module Name:     EXTENDER
+// Module Name:     IMM_EXTENDER
 // Project Name:    RISC-V
 // Target Devices: 
 // Tool Versions: 
@@ -27,6 +27,7 @@ module IMM_EXTENDER #(
         parameter SB_FORMAT     = 3'b011 ,
         parameter UJ_FORMAT     = 3'b100 
     ) (
+		input			 CLK		,	
         input   [24 : 0] IMM_INPUT  ,
         input   [2  : 0] IMM_FORMAT ,
         output  [31 : 0] IMM_OUTPUT  
@@ -34,44 +35,44 @@ module IMM_EXTENDER #(
     
     reg [31 : 0] IMM_OUTPUT_REG;
     
-    always@(*)
+    always@(posedge CLK)
     begin
         case(IMM_FORMAT)
             I_FORMAT:
             begin 
                 if(IMM_INPUT[24] == 1'b1)
-                    IMM_OUTPUT_REG = {20'b11111111111111111111,IMM_INPUT[24:13]};
+                    IMM_OUTPUT_REG <= {20'b11111111111111111111,IMM_INPUT[24:13]};
                 else
-                    IMM_OUTPUT_REG = {20'b0,IMM_INPUT[24:13]};
+                    IMM_OUTPUT_REG <= {20'b0,IMM_INPUT[24:13]};
             end
             S_FORMAT:
             begin 
                 if(IMM_INPUT[24]==1'b1)
-                    IMM_OUTPUT_REG = {20'b11111111111111111111,IMM_INPUT[24:18],IMM_INPUT[4:0]};
+                    IMM_OUTPUT_REG <= {20'b11111111111111111111,IMM_INPUT[24:18],IMM_INPUT[4:0]};
                 else
-                    IMM_OUTPUT_REG = {20'b0,IMM_INPUT[24:18],IMM_INPUT[4:0]};
+                    IMM_OUTPUT_REG <= {20'b0,IMM_INPUT[24:18],IMM_INPUT[4:0]};
             end
             U_FORMAT:   
             begin    
-                IMM_OUTPUT_REG = {IMM_INPUT[24:5],12'b0};
+                IMM_OUTPUT_REG <= {IMM_INPUT[24:5],12'b0};
             end
             SB_FORMAT:                
             begin
                 if(IMM_INPUT[24]==1'b1)
-                     IMM_OUTPUT_REG = {20'b11111111111111111111,IMM_INPUT[0],IMM_INPUT[23:18],IMM_INPUT[4:1],1'b0};
+                    IMM_OUTPUT_REG <= {20'b11111111111111111111,IMM_INPUT[0],IMM_INPUT[23:18],IMM_INPUT[4:1],1'b0};
                 else
-                     IMM_OUTPUT_REG = {20'b0,IMM_INPUT[0],IMM_INPUT[23:18],IMM_INPUT[4:1],1'b0};
+                    IMM_OUTPUT_REG <= {20'b0,IMM_INPUT[0],IMM_INPUT[23:18],IMM_INPUT[4:1],1'b0};
             end
             UJ_FORMAT:            
             begin
-                 if(IMM_INPUT[24]==1'b1)
-                     IMM_OUTPUT_REG = {12'b111111111111,IMM_INPUT[12:5],IMM_INPUT[13],IMM_INPUT[23:18],IMM_INPUT[17:14],1'b0};
-                 else
-                     IMM_OUTPUT_REG = {12'b0,IMM_INPUT[12:5],IMM_INPUT[13],IMM_INPUT[23:18],IMM_INPUT[17:14],1'b0};
+                if(IMM_INPUT[24]==1'b1)
+                    IMM_OUTPUT_REG <= {12'b111111111111,IMM_INPUT[12:5],IMM_INPUT[13],IMM_INPUT[23:18],IMM_INPUT[17:14],1'b0};
+                else
+                    IMM_OUTPUT_REG <= {12'b0,IMM_INPUT[12:5],IMM_INPUT[13],IMM_INPUT[23:18],IMM_INPUT[17:14],1'b0};
             end
             default:
             begin
-                IMM_OUTPUT_REG = 32'b0;
+                IMM_OUTPUT_REG <= 32'b0;
             end
         endcase
     end
---
+++ src/write_back/Multiplexer_2_to_1.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:        Muhammad Ijaz
+// 
+// Create Date:     05/06/2017 08:43:21 AM
+// Design Name: 
+// Module Name:     Multiplexer_2_to_1
+// Project Name:    RISC-V
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Multiplexer_2_to_1#(
+        parameter BUS_WIDTH     = 32       
+    ) (
+        input   [BUS_WIDTH - 1 : 0] IN1     ,
+        input   [BUS_WIDTH - 1 : 0] IN2     ,
+        input                       SELECT  ,
+        output  [BUS_WIDTH - 1 : 0] OUT     
+    );
+    
+    reg [BUS_WIDTH - 1 : 0] OUT_REG;
+        
+    always@(*)
+    begin
+        case(SELECT)
+            1'b0:
+            begin
+                OUT_REG = IN1;
+            end
+            1'b1:
+            begin
+                OUT_REG = IN2;
+            end
+        endcase
+    end 
+    
+    assign OUT = OUT_REG;
+    
+endmodule
