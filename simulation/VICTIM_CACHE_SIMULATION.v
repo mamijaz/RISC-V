c493699e01414793bceee1fd1887fdadb776b3d1
mamijaz <ihjaaz@gmail.com>
2017-08-15 01:55:22 +0530

    Victim Cache Devolopments

---
+++ simulation/VICTIM_CACHE_SIMULATION.v
@@ -0,0 +1,84 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:        Muhammad Ijaz
+// 
+// Create Date:     08/15/2017 01:21:09 AM
+// Design Name: 
+// Module Name:     VICTIM_CACHE_SIMULATION
+// Project Name:    RISC-V
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module VICTIM_CACHE_SIMULATION();
+
+    parameter   BLOCK_WIDTH             = 512                           ;
+    parameter   TAG_WIDTH               = 26                            ;
+    parameter   MEMORY_LATENCY    	    = "HIGH_LATENCY"                ;
+    
+    localparam  MEMORY_DEPTH            = 4                             ;
+    localparam  ADDRESS_WIDTH           = $clog2(MEMORY_DEPTH-1)        ;    
+    
+    // Inputs
+    reg                                clk                              ;
+    reg    [TAG_WIDTH - 1      : 0]    write_tag_address                ;   
+    reg    [BLOCK_WIDTH - 1    : 0]    write_data                       ;
+    reg                                write_enable                     ;
+    reg    [TAG_WIDTH - 1      : 0]    read_tag_address                 ;                                         
+    reg                                read_enble                       ;
+    
+    // Outputs
+    wire                               read_hit                         ;                                                    
+    wire   [BLOCK_WIDTH - 1    : 0]    read_data                        ;
+    
+    // Instantiate the Unit Under Test (UUT)
+    VICTIM_CACHE #(
+        .BLOCK_WIDTH(BLOCK_WIDTH),
+        .TAG_WIDTH(TAG_WIDTH),
+        .MEMORY_LATENCY(MEMORY_LATENCY)
+    )uut(
+        .CLK(clk),
+        .WRITE_TAG_ADDRESS(write_tag_address),   
+        .WRITE_DATA(write_data),
+        .WRITE_ENABLE(write_enable),
+        .READ_TAG_ADDRESS(read_tag_address),                                         
+        .READ_ENBLE(read_enble), 
+        .READ_HIT(read_hit),                                                    
+        .READ_DATA(read_data)           
+        );
+        
+    initial 
+    begin
+        // Initialize Inputs
+        clk                      = 1'b0 ;
+
+        // Wait 100 ns for global reset to finish
+        #100;
+        
+        // Add stimulus here
+        clk                      = 1'b1 ;
+        #100;
+        clk                      = 1'b0 ;
+        #100;
+        clk                      = 1'b1 ;
+        #100;
+        clk                      = 1'b0 ;
+        #100;
+        clk                      = 1'b1 ;
+        #100;
+        clk                      = 1'b0 ;
+        #100;
+        clk                      = 1'b1 ;
+    end
+    
+endmodule
--- src/riscv_instruction_cache/VICTIM_CACHE.v
+++ src/riscv_instruction_cache/VICTIM_CACHE.v
@@ -22,61 +22,129 @@
 
 module VICTIM_CACHE #(
         parameter   BLOCK_WIDTH             = 512                           ,
-        parameter   MEMORY_DEPTH            = 8                             ,
         parameter   TAG_WIDTH               = 26                            ,
         parameter   MEMORY_LATENCY    	    = "HIGH_LATENCY"                ,
         
+        localparam  MEMORY_DEPTH            = 4                             ,
         localparam  ADDRESS_WIDTH           = clog2(MEMORY_DEPTH-1)        	                           
     ) (
         input                              CLK                              ,
-        input  [ADDRESS_WIDTH - 1  : 0]    WRITE_TAG_ADDRESS                ,   
-        input  [BLOCK_WIDTH - 1    : 0]    DATA_IN                          ,
+        input  [TAG_WIDTH - 1      : 0]    WRITE_TAG_ADDRESS                ,   
+        input  [BLOCK_WIDTH - 1    : 0]    WRITE_DATA                       ,
         input                              WRITE_ENABLE                     ,
-        input  [ADDRESS_WIDTH - 1  : 0]    READ_TAG_ADDRESS                 ,                                         
+        input  [TAG_WIDTH - 1      : 0]    READ_TAG_ADDRESS                 ,                                         
         input                              READ_ENBLE                       , 
         output                             READ_HIT                         ,                                                    
-        output [BLOCK_WIDTH - 1    : 0]    DATA_OUT           
+        output [BLOCK_WIDTH - 1    : 0]    READ_DATA           
     );
     
-    reg [BLOCK_WIDTH - 1    : 0]    memory              [MEMORY_DEPTH - 1   : 0]    ;
     reg [TAG_WIDTH - 1      : 0]    tag                 [MEMORY_DEPTH - 1   : 0]    ;
-    reg [ADDRESS_WIDTH - 1  : 0]    lru                 [MEMORY_DEPTH - 1   : 0]    ;
+    reg [BLOCK_WIDTH - 1    : 0]    memory              [MEMORY_DEPTH - 1   : 0]    ;
     
     reg                             read_hit_out_reg_1                              ;
     reg [BLOCK_WIDTH - 1    : 0]    data_out_reg_1                                  ;
+    
+    reg [ADDRESS_WIDTH - 1  : 0]    record_counter                                  ;                        
+    
+    wire                            full                                            ;
+    wire                            empty                                           ;
+    
+    assign full     = ( record_counter == (MEMORY_DEPTH - 1) )                      ;
+    assign empty    = ( record_counter == 0 )                                       ;
     
     integer i;
     initial
     begin
         for (i = 0; i < MEMORY_DEPTH; i = i + 1)
-            memory [ i ] = {BLOCK_WIDTH{1'b0}};
-        for (i = 0; i < MEMORY_DEPTH; i = i + 1)
             tag [ i ] = {TAG_WIDTH{1'b0}};
         for (i = 0; i < MEMORY_DEPTH; i = i + 1)
-            lru [ i ] = {ADDRESS_WIDTH{1'b0}};
-    end
-    
-    always@(*)
-    begin
+            memory [ i ] = {BLOCK_WIDTH{1'b0}};
+        record_counter  = {ADDRESS_WIDTH{1'b0}};
     end
     
     always @(posedge CLK)
     begin
-        if (WRITE_ENABLE)
+        if (WRITE_ENABLE & !full)
         begin
-           
+            tag [ record_counter ]      <= WRITE_TAG_ADDRESS    ;
+            memory [ record_counter ]   <= WRITE_DATA           ;
+            record_counter              <= record_counter + 1   ;   
         end
-        if (READ_ENBLE)
+        if (WRITE_ENABLE & full)
         begin
-            
+            for (i = 0; i < MEMORY_DEPTH - 1 ; i = i + 1)
+            begin
+                tag [ i ]       <= tag [ i + 1 ]    ;
+                memory [ i ]    <= memory [ i + 1 ] ;
+            end
+            tag [ record_counter ]      <= WRITE_TAG_ADDRESS    ;
+            memory [ record_counter ]   <= WRITE_DATA           ;
+        end
+        
+        if (READ_ENBLE & !empty)
+        begin
+            if( tag [ 0 ] == READ_TAG_ADDRESS) 
+            begin
+                read_hit_out_reg_1  <= 1'b1                     ;
+                data_out_reg_1      <= memory [ 0 ]             ;
+                record_counter      <= record_counter - 1       ;
+                for (i = 0; i < MEMORY_DEPTH - 1 ; i = i + 1)
+                begin
+                    tag [ i ]       <= tag [ i + 1 ]    ;
+                    memory [ i ]    <= memory [ i + 1 ] ;
+                end
+            end
+            else if( tag [ 1 ] == READ_TAG_ADDRESS) 
+            begin
+                read_hit_out_reg_1  <= 1'b1                     ;
+                data_out_reg_1      <= memory [ 1 ]             ;
+                record_counter      <= record_counter - 1       ;
+                for (i = 1; i < MEMORY_DEPTH - 1 ; i = i + 1)
+                begin
+                    tag [ i ]       <= tag [ i + 1 ]    ;
+                    memory [ i ]    <= memory [ i + 1 ] ;
+                end
+            end
+            else if( tag [ 2 ] == READ_TAG_ADDRESS) 
+            begin
+                read_hit_out_reg_1  <= 1'b1                     ;
+                data_out_reg_1      <= memory [ 2 ]             ;
+                record_counter      <= record_counter - 1       ;
+                for (i = 2; i < MEMORY_DEPTH - 1 ; i = i + 1)
+                begin
+                    tag [ i ]       <= tag [ i + 1 ]    ;
+                    memory [ i ]    <= memory [ i + 1 ] ;
+                end
+            end
+            else if( tag [ 3 ] == READ_TAG_ADDRESS) 
+            begin
+                read_hit_out_reg_1  <= 1'b1                     ;
+                data_out_reg_1      <= memory [ 3 ]             ;
+                record_counter      <= record_counter - 1       ;
+                for (i = 3; i < MEMORY_DEPTH - 1 ; i = i + 1)
+                begin
+                    tag [ i ]       <= tag [ i + 1 ]    ;
+                    memory [ i ]    <= memory [ i + 1 ] ;
+                end
+            end
+            else
+            begin
+                read_hit_out_reg_1  <= 1'b0                         ;
+                data_out_reg_1      <= {BLOCK_WIDTH{1'b0}}          ;
+            end
+        end
+        if (READ_ENBLE & empty)
+        begin
+            read_hit_out_reg_1  <= 1'b0                         ;
+            data_out_reg_1      <= {BLOCK_WIDTH{1'b0}}          ;
         end
     end
     
     generate
         if (MEMORY_LATENCY == "LOW_LATENCY") 
         begin
-            assign READ_HIT = read_hit_out_reg_1    ;
-            assign DATA_OUT = data_out_reg_1        ;  
+            assign READ_HIT     = read_hit_out_reg_1    ;
+            assign READ_DATA    = data_out_reg_1        ;  
         end 
         else 
         begin
@@ -90,8 +158,8 @@ module VICTIM_CACHE #(
                     data_out_reg_2      <= data_out_reg_1       ;
                 end
             end  
-            assign READ_HIT = read_hit_out_reg_2    ;  
-            assign DATA_OUT = data_out_reg_2        ;
+            assign READ_HIT     = read_hit_out_reg_2    ;  
+            assign READ_DATA    = data_out_reg_2        ;
         end
     endgenerate
     
